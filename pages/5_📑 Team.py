9a21a482523be328b84ffc946c3ce8649b516710
INGEN-HOPE <96059887+INGEN-HOPE@users.noreply.github.com>
2025-04-03 11:57:10 +0530

    Update 5_📑 Team.py

--- pages/5_📑 Team.py
+++ pages/5_📑 Team.py
@@ -70,10 +70,15 @@ team_lead = {
 # Team Members
 team_members = [
     {"name": "Priyanka Mehta", "photo": "pages/images/priyanka.jpg", "linkedin": "https://www.linkedin.com/in/priyanka-mehta-92339a355", "twitter": "https://x.com/priyankammehta", "github": "https://github.com/priyankamehta1811"},
+    {"name": "Varsha Ravi", "photo": "", "linkedin": "#", "twitter": "#", "github": "#"},
+    {"name": "Ramakant Mohite", "photo": "", "linkedin": "#", "twitter": "#", "github": "#"},
+    {"name": "M D Imran", "photo": "", "linkedin": "#", "twitter": "#", "github": "#"},
+    {"name": "Jyoti Soni", "photo": "pages/images/jyoti.jpg", "linkedin": "#", "twitter": "https://x.com/Sonijyoti63", "github": "#"},
     {"name": "Balendu Upmanyu", "photo": "pages/images/Balendu.jpg", "linkedin": "https://www.linkedin.com/in/balendu-upmanyu-64b684217", "twitter": "", "github": "#"},
     {"name": "Keerti Aswin", "photo": "pages/images/Aswin.jpg", "linkedin": "#", "twitter": "https://x.com/keerthic_aswin", "github": "#"},
-    {"name": "Jyoti Soni", "photo": "pages/images/jyoti.jpg", "linkedin": "#", "twitter": "https://x.com/Sonijyoti63", "github": "#"},
     {"name": "Raj Rajeshwar Chowdhry", "photo": "pages/images/jyoti.jpg", "linkedin": "#", "twitter": "#", "github": "#"},
+    {"name": "", "photo": "", "linkedin": "#", "twitter": "#", "github": "#"},
+    {"name": "", "photo": "", "linkedin": "#", "twitter": "#", "github": "#"},
 ]
 
 # Custom CSS for Proper Vertical Alignment
