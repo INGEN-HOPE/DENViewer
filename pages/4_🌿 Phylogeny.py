d3420a310be08d2292a41f8ee9355c26481c7000
INGEN-HOPE <96059887+INGEN-HOPE@users.noreply.github.com>
2025-04-04 02:09:11 +0530

    Update 4_🌿 Phylogeny.py

--- pages/4_🌿 Phylogeny.py
+++ pages/4_🌿 Phylogeny.py
@@ -174,7 +174,6 @@ st.markdown(
     <hr>
     <p style='text-align: center;'>
     © 2024 Rajesh Pandey | Ingen-hope Lab
-   Some visualizations may take time to load due to the complexity of the data. Please be patient while the plots generate.
     </p>
     """,
     unsafe_allow_html=True
